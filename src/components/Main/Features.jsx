b79e192b9a66b62d15f964a9a7aea19262074fcc
Johnny <johnnyomoseni100@gmail.com>
2024-01-15 23:22:46 +0100

    feat: update features section

--- src/components/Main/Features.jsx
+++ src/components/Main/Features.jsx
@@ -1,10 +1,39 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+
 function Features() {
   return (
-    <div
-      className="bg-[
-#FFF0BD]"
-    >
+    <div className="bg-[#FFF0BD]">
       Features
+      <div className="blogs-content">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 3000,
+          }}
+          speed={800}
+          slidesPerView={1}
+          spaceBetween={16}
+          breakpoints={{
+            800: {
+              slidesPerView: 3,
+            },
+            500: {
+              slidesPerView: 2,
+            },
+          }}
+          loop
+          className="blog-swiper"
+        >
+          {blogSlice.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                {/* <BlogCard {...item} /> */}
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </div>
   );
 }
